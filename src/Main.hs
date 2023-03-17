27bb1fad51af389bc84b7b7dd713d22806174b6f
silky <noonsilk+-noonsilk@gmail.com>
2023-03-17 21:30:29 +0000

    truly terrible logo

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,6 @@ import Diagrams.Backend.Cairo.CmdLine
 import Diagrams.Prelude
 import Diagrams.TwoD.Offset
 import Diagrams.TwoD.Path.Boolean qualified as Boolean
--- import System.Random
 
 
 main :: IO ()
@@ -12,23 +11,36 @@ main = mainWith haskellLogo
 
 
 haskellLogo :: Diagram B
-haskellLogo =
-  Path trails
-    # expandPath 0.2
-    # Boolean.union Winding
-    # stroke
-    # centerXY
+haskellLogo = d <> (square 5 # lw none)
   where
-    verts  = (map . map) p2 haskellPoints
-    trails = map fromVertices verts
+    d = (mempty
+        <> lambdas
+        <> eqsD # translateX (1.1)
+        ) # centerXY
+    lambdas
+      = (asPath haskellPoints)
+          # stroke
+          # lw 50
+          # centerXY
+          # clipBy (square 5 # translateY (1.2))
+          # clipBy (square 5 # translateY (-1.2))
+    eqs =
+      [ [ (2.3, 1.89), (4, 1.89) ]
+        , [ (2.7, 1.30), (4, 1.30) ]
+      ]
+    eqsP = Path $ map fromVertices $ (map . map) p2 eqs
+    eqsD = mempty
+            <> x # stroke # lc white # lw 50 # translateY (0.2)
+            <> eqsP # stroke # lw 50 # lc black # centerXY
+    x = asPath [farLine'] # centerXY # translateX (-0.4) # translateY (-0.4)
+    asPath = Path . map fromVertices . (map . map) p2
+    farLine' = [ (0, 3/2), (0 + (1 * 1.2), 0) ]
+    farLine = [ (0.8, 3), (0.8 + (2 * 1.2), 0) ]
     haskellPoints
       = [ -- >
         [ (0, 3), (1.2, 1.5), (0,0) ]
         -- \
-        , [ (0.8, 3), (0.8 + (2 * 1.2), 0) ]
+        , farLine
         -- /
         , [ (0.8, 0), (0.8 + 1.2, 1.5) ]
-        -- =
-        , [ (2.2, 1.85), (4, 1.85) ]
-        , [ (2.7, 1.32), (4, 1.32) ]
         ]
